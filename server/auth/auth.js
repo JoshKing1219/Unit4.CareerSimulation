68ae629da20da907d6a64c509c9086e63a424d18
JoshKing1219 <joshk7951@gmail.com>
2024-08-09 11:50:19 -0400

    Troubleshooting the register endpoint

---
+++ server/auth/auth.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { createUser, getByUsername } = require("../db/users");
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req, res) => {
+  try {
+    //username, password --> req.body
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide a username and password" });
+    }
+
+    //checks if the username is taken by using the getByUsername function
+    const user = await getByUsername(username);
+
+    if (user) {
+      return res.status(409).send({
+        message: "That username has already been taken.",
+      });
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT) || 42
+    );
+
+    //create user in db
+    const newUser = await createUser({ ...req.body, password: hashedPassword });
+
+    //create token for user
+    const token = jwt.sign(
+      { id: newUser.id },
+      process.env.JWT || "super duper secret"
+    );
+
+    //send response
+    res.status(201).send({ token });
+  } catch (error) {
+    //error catching
+    console.log(error);
+    res.status(500).send({ error, message: "Could not register user." });
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    //Finds the user by the username given
+    const user = await getByUsername(req.body.username);
+
+    //Checks if the password matches
+    const isSamePassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    //Throws a message if any login credentials are incorrect and prompts the user to try again
+    if (!user || !isSamePassword) {
+      return res
+        .status(401)
+        .send("Your login credentials were incorrect. Please try again.");
+    }
+
+    //Creates and sends a token with the user id so the user can access authenticated routes
+    const token = jwt.sign(
+      { id: user.id },
+      process.env.JWT || "super duper secret"
+    );
+
+    res.send({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error, message: "Could not login the user." });
+  }
+});
+
+module.exports = authRouter;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 server.get("/", (req, res) => {
   res.send({ message: "Running..." });
 });
+
+server.use("/api", require("./api/index"));
 
 module.exports = server;
